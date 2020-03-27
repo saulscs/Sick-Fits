ee74b82f122d34ac27b62bb9337efded607097f4
saulscs <saulcoronasantos@ymail.com>
2020-03-26 23:57:45 -0600

    Inial SetUp

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const Home = props => (
+    
+    <div>
+        <h1>Hey</h1>
+    </div>
+    
+)
+
+export default Home
---
+++ frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Sell = props => (
+    
+    <div>
+        <h2>Sell</h2>
+    </div>
+    
+)
+
+export default Sell
