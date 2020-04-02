c6e375bf0b635eac13235f1c5b46adcfb38a6a0c
saulscs <saulcoronasantos@ymail.com>
2020-04-02 14:49:42 -0600

    Visualizing Route Changes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,21 @@
 import Nav from './Nav'
 import Link from 'next/link'
 import styled from 'styled-components'
+import Router from 'next/router'
+import Nprogress from 'nprogress'
+
+
+Router.onRouteChangeStart = () => {
+   Nprogress.start();
+}
+
+Router.onRouteChangeComplete = () => {
+    Nprogress.done()
+}
+
+Router.onRouteChangeError = () => {
+    Nprogress.done()
+}
 
 const Logo = styled.h1 `
     font-size: 4rem;
