23ddf5a574c948703f262018761b9044786edb05
saulscs <saulcoronasantos@ymail.com>
2020-04-05 21:22:07 -0500

    Add first query and mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
-const Mutations = {};
+const Mutations = {
+    createDog(parent, args, ctx,info){
+        global.dogs = global.dogs || []
+        //create dogs
+        const newDog = {name: args.name}
+        console.log(args)
+        return newDog
+    }
+};
 
 module.exports = Mutations;
