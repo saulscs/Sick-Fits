6919a6f4a402ebf8cde593424e92cb12516d5d0d
saulscs <saulcoronasantos@ymail.com>
2020-04-24 21:50:55 -0500

    Data Relationship

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,8 +8,17 @@ const Mutations = {
     async createItem (parent, args, ctx,info){
         //TODO: check if they are logged in
         console.log(args)
+        if(!ctx.request.userId){
+            throw new Error('You must be logged in to do that')
+        }
         const item = await ctx.db.mutation.createItem({
             data:{
+                // This is how to create a relationship between the item and the user
+                user:{
+                    connect: {
+                        id: ctx.request.userId
+                    }
+                },
                 ...args
             }
         }, info)
