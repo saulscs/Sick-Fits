777c2a03eb18e4519b44b98f8de1e0160c58d0f5
saulscs <saulcoronasantos@ymail.com>
2020-04-28 19:39:46 -0500

    Permission management Part 2

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,6 +1,19 @@
 import {Query} from 'react-apollo'
 import Error from './ErrorMessage'
 import gql from 'graphql-tag'
+import Table from './styles/Table'
+import SickButton from './styles/SickButton'
+
+const possiblePermissions = [ 
+    'ADMIN',
+    'USER',
+    'ITEMCREATE',
+    'ITEMUPDATE',
+    'ITEMDELETE',
+    'PERMISSIONSUPDATE',
+
+];
+
 
 const ALL_USERS_QUERY = gql `
     query {
@@ -15,13 +28,51 @@ const ALL_USERS_QUERY = gql `
 
 const Permissions = props => (
     <Query query={ALL_USERS_QUERY}>
-        {({data, loading, error}) => (
+        {({data, loading, error}) => console.log(data) ||  (
             <div>
                 <Error error={error}/>
-                <p>Hey</p>
+                <h2>Manage Permissions</h2>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            {possiblePermissions.map(permissions => 
+                                <th>{permissions}</th>)}
+
+                            <th>👇</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.users.map( user => <User user={user}/>)}
+                    </tbody>
+                </Table>
             </div>
         )}
     </Query>
 )
+
+class User extends React.Component {
+    render() {
+        const user = this.props.user
+        return (
+            <tr>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                {possiblePermissions.map(permission => (
+                    <td>
+                        <label htmlFor={`${user.id}-permission-${permission}`}>
+                            <input type="checkbox"/>
+                        </label>
+                    </td>
+                ))};
+                <td>
+                    <SickButton>Update</SickButton>
+                </td>
+            </tr>
+        );
+    };
+};
+
 
 export default Permissions
