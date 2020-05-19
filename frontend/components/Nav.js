616f1066c4c45a83481a57abb627ed83cedd2959
saulscs <saulcoronasantos@ymail.com>
2020-05-18 22:58:15 -0500

    Animating our cart count component

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -4,6 +4,7 @@ import {TOGGLE_CART_MUTATION} from './Cart'
 import NavStyles from './styles/NavStyles'
 import User from './User'
 import Sigout from './Signout'
+import CartCount from './CartCount'
 
 const Nav = () => (
    
@@ -27,7 +28,12 @@ const Nav = () => (
              <Sigout/>
              <Mutation mutation={TOGGLE_CART_MUTATION}>
                 {(toggleCart) => (
-                    <button onClick={toggleCart}>My Cart</button>
+                    <button onClick={toggleCart}>
+                        My Cart
+                        <CartCount count={me.cart.reduce((tally, 
+                            cartItem) => tally + cartItem.quantity, 0)
+                        }></CartCount>
+                    </button>
                 )}
              </Mutation>
              </>
